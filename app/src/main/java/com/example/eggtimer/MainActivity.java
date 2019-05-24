198dd9d696987d201ff7403247e9390138a2e823
Irmina Stepien <irm.stepien@gmail.com>
2019-05-24 13:49:37 +0200

    SeekBar connected with timeTextView

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -3,11 +3,51 @@ package com.example.eggtimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+
+        Button startButton = findViewById(R.id.startButton);
+        Button stopButton = findViewById(R.id.stopButton);
+
+        int maxTime = 10;
+        int startingPosition = 0;
+
+        seekBar.setMax(maxTime);
+        seekBar.setProgress(startingPosition);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("info", "progres" + progress);
+                setTimeTextView(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+    public void setTimeTextView(int progres){
+       TextView timeTextView = findViewById(R.id.timeTextView);
+        timeTextView.setText(String.valueOf(progres));
     }
 }
