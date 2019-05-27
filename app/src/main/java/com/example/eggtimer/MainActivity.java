ceb58ea9e6eb05c1d90051d92ef1e644af4e2d80
Irmina Stepien <irm.stepien@gmail.com>
2019-05-27 09:34:16 +0200

    updateTimer method extracted

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.eggtimer;
 
-import android.os.CountDownTimer;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.util.Log;
+
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -12,17 +13,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView timeTextView;
+    SeekBar seekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar seekBar = findViewById(R.id.seekBar);
-
-        Button startButton = findViewById(R.id.startButton);
-        Button stopButton = findViewById(R.id.stopButton);
-
-       final TextView timeTextView = findViewById(R.id.timeTextView);
+        timeTextView = findViewById(R.id.timeTextView);
+        seekBar = findViewById(R.id.seekBar);
 
         int maxTime = 600;
         int startingPosition = 0;
@@ -34,10 +34,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 Log.i("info", "progress" + progress);
-                int minutes = progress/60;
-                int seconds = progress-(minutes*60);
-
-                timeTextView.setText(checkDigit(minutes)+":"+checkDigit(seconds));
+                updateTimer(progress);
 
 
             }
@@ -54,8 +51,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        timeTextView.setText(checkDigit(minutes) + ":" + checkDigit(seconds));
+    }
+
     public String checkDigit(int number) {
         return number <= 9 ? "0" + number : String.valueOf(number);
     }
+
 
 }
