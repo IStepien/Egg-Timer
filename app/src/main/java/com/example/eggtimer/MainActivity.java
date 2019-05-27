58a4b0fe2519f3e82d222a8c62edb4b3a6e96384
Irmina Stepien <irm.stepien@gmail.com>
2019-05-27 11:13:34 +0200

    layout modified

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -5,9 +5,6 @@ import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.util.Log;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
